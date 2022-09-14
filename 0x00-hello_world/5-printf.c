d791a9184e82bbd9934d19bc9adef548a9f8174d
OBIAGWUNNAMDI1 <obiagwunathan@gmail.com>
2022-09-14 06:28:47 -0700

    a script that prints with the printf keyword

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -print the string in the function
+ * Description:using the main function
+ * This program that prints with the printf
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
