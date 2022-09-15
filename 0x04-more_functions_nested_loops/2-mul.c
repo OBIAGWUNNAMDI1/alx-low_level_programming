8ffa2ad6355bcba8c7cd614371dfa3b7193fc363
OBIAGWUNNAMDI1 <obiagwunathan@gmail.com>
2022-09-15 09:17:59 -0700

    multiplies two intger type

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul -function that multiples two integers
+ * @a: the first integer type input
+ * @b: the second integer type input
+ * Description:Helps to do multplication between two int types
+ * Return:0
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
