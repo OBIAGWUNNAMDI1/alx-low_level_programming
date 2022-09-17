c8dee0752fac75dfc6a252ebe22eec093f8b6ed1
OBIAGWUNNAMDI1 <obiagwunathan@gmail.com>
2022-09-17 06:18:03 -0700

    prints triangle and a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,23 +6,23 @@
  */
 void print_triangle(int size)
 {
-	int i;
-	int j;
-	int k;
+	int i = 0, j, n = size - 1;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
 	}
 	else
-	{
-		for (i = 1; i <= size; i++)
-		{
-			for (j = size - i ; j > 0; j--)
-			{
-				_putchar(ut
-			}
-		}
-
-	}
+		_putchar('\n');
 }
