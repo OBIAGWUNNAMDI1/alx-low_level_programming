8cc5ead5782fd25b253fb06cb6d39e121829e514
OBIAGWUNNAMDI1 <obiagwunathan@gmail.com>
2022-09-19 12:15:50 -0700

    swapping integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int -function to swap integers
+ * @a:declaration of a and paramters for the function swap_int
+ * @b:declaration of b and parameters for the function swap_int
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	a = 98;
+	b = 402;
+
+	a = *b;
+	b = *a;
+}
