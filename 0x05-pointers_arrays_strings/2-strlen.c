5f178119a5617a67dbb578650b985a582aea78e0
OBIAGWUNNAMDI1 <obiagwunathan@gmail.com>
2022-09-19 12:26:04 -0700

    checking string of length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - function that returns length of string
+ * @s : a char values
+ * Return : 0
+ */
+void _strlen(char *s)
+{
+	int p;
+
+	for (p = 0; s[p] != 0; p++)
+	{
+	}
+	return (p);
+}
